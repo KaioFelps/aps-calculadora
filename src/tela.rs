129eefb5eed466d1dc3785287d2fb5f839094b6e
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-11-26 23:42:42 -0300

    feat: adiciona métodos `adicione` e `limpe` à `Tela`

--- src/tela.rs
+++ src/tela.rs
@@ -1,1 +1,13 @@
+use crate::tecla::Digito;
+
 pub struct Tela;
+
+impl Tela {
+    pub fn adicione(digito: Digito) {
+        print!("{}", digito.to_char());
+    }
+
+    pub fn limpe() {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    }
+}
