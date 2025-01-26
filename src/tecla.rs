114ae47596d7b717bf86efa2ed3c772f8a82406c
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-26 02:00:42 -0300

    feat(TeclaKaio): implement missing `Tecla` trait methods

--- src/tecla.rs
+++ src/tecla.rs
@@ -1,28 +1,63 @@
+use crate::enums::Ação;
+use crate::traits::{DynamicMutable, IntoDynamicMutable, Tecla, Teclado, UnwrapDynamicMutableRef};
+use std::sync::{RwLockReadGuard, RwLockWriteGuard};
 use std::{rc::Rc, sync::RwLock};
-
-use crate::traits::{DynamicMutable, IntoDynamicMutable, Tecla, Teclado};
 
 pub struct TeclaKaio {
     teclado: Option<DynamicMutable<Box<dyn Teclado>>>,
+    ação: Option<Ação>,
+    chave: String,
 }
 
 impl TeclaKaio {
-    pub fn new() -> Self {
-        Self { teclado: None }
+    pub fn new(chave: &str) -> Self {
+        Self {
+            teclado: None,
+            ação: None,
+            chave: chave.to_string(),
+        }
+    }
+
+    pub fn com_ação(mut self, ação: Ação) -> Self {
+        self.ação = Some(ação);
+        self
     }
 }
 
 impl Tecla for TeclaKaio {
     fn pressione(&self) {
-        todo!()
+        let ação = self.ação
+            .as_ref()
+            .expect("Tecla sem ação acionada. Adicione uma ação ou remova a tecla para que o programa funcione corretamente.");
+
+        self.teclado
+            .as_ref()
+            .unwrap()
+            .read()
+            .unwrap()
+            .receba_ação(ação);
     }
 
     fn defina_teclado(&mut self, teclado: DynamicMutable<Box<dyn Teclado>>) {
         self.teclado = Some(teclado);
     }
 
-    fn obtenha_teclado(&self) -> Option<DynamicMutable<Box<dyn Teclado>>> {
-        self.teclado.clone()
+    fn obtenha_teclado(&self) -> &Option<DynamicMutable<Box<dyn Teclado>>> {
+        &self.teclado
+    }
+
+    fn defina_ação(&mut self, ação: Ação) {
+        self.ação = Some(ação);
+    }
+
+    fn obtenha_ação(&self) -> &Ação {
+        self.ação.as_ref().expect(
+            "Tecla sem ação acionada. Adicione uma ação ou remova a tecla para que o programa funcione corretamente.",
+        )
+    }
+
+    fn é(&self, chave: &str) -> bool {
+        self.chave.eq(chave)
     }
 }
 
